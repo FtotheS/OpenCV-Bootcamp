e94279b0ae446728bab4cb533e53b12fcc2c4485
FtotheS <farmansiddique9003@gmail.com>
2022-11-07 18:54:26 +0800

    updated

--- session2/Editing_converting_images.py
+++ session2/Editing_converting_images.py
@@ -1,6 +1,33 @@
 import cv2
 import numpy as np
 
+
+img = cv2.imread ('Resources/lena.png')
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   #convert to gray scale
+img_blur = cv2.GaussianBlur(img, (7,7), 0)         #convert to blur
+img_canny = cv2.Canny(img, 100, 100)               #convert to canny
+
+print (img.shape)
+print (img_gray.shape)
+print (img_blur.shape)
+print (img_canny.shape)
+
+cv2.imshow ('Color', img)
+cv2.imshow ('Gray', img_gray)
+cv2.imshow ('Blur', img_blur)
+cv2.imshow ('Canny', img_canny)
+
+cv2.waitKey (0)
+
+
+
+
+
+
+
+
+
+
 #convert to gray scale
 
 # img = cv2.imread ('Resources/lena.png')
@@ -49,12 +76,12 @@ import numpy as np
 
 #convert to erode
 
-kernel = np.ones ((5,5), np.uint8)
-print (kernel)
-img = cv2.imread ('Resources/lena.png')
-img_erode = cv2.erode (img, kernel, iterations=2)
-print (img.shape)
-print (img_erode.shape)
-cv2.imshow ('Color', img)
-cv2.imshow ('Eroded', img_erode)
-cv2.waitKey(0)
+# kernel = np.ones ((5,5), np.uint8)
+# print (kernel)
+# img = cv2.imread ('Resources/lena.png')
+# img_erode = cv2.erode (img, kernel, iterations=2)
+# print (img.shape)
+# print (img_erode.shape)
+# cv2.imshow ('Color', img)
+# cv2.imshow ('Eroded', img_erode)
+# cv2.waitKey(0)
--- session2/Reading_images_videos_webcam.py
+++ session2/Reading_images_videos_webcam.py
@@ -9,25 +9,25 @@
 
 # reading a video
 
-# import cv2
-# cap = cv2.VideoCapture ('Resources/elon.mp4')
-# while True:
-#     success, img = cap.read()
-#     print (img.shape)
-#     cv2.imshow ('Output', img)
-#     if cv2.waitKey(1) & 0xFF == ord ('q'):
-#         break
-
-# reading the webcam
-
 import cv2
-cap = cv2.VideoCapture (0) #default webcam
-cap.set (3, 1280)
-cap.set (4, 720)
+cap = cv2.VideoCapture ('Resources/elon.mp4')
 while True:
     success, img = cap.read()
     print (img.shape)
     cv2.imshow ('Output', img)
     if cv2.waitKey(1) & 0xFF == ord ('q'):
         break
+
+# reading the webcam
+
+# import cv2
+# cap = cv2.VideoCapture (0) #default webcam
+# cap.set (3, 1280)
+# cap.set (4, 720)
+# while True:
+#     success, img = cap.read()
+#     print (img.shape)
+#     cv2.imshow ('Output', img)
+#     if cv2.waitKey(1) & 0xFF == ord ('q'):
+#         break
 
